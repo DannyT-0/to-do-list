a989181233b89ff9c91291327c1b41e53c64d334
danny <dannytokarev@gmail.com>
2022-11-14 12:29:45 -0500

    minor tweaks

--- src/projects.js
+++ src/projects.js
@@ -8,6 +8,12 @@ const cancelButton = document.querySelector(".cancelBtn");
 const todoItems = document.querySelector(".list");
 const listItems = document.querySelector(".listItems");
 
+const taskTitle = document.querySelector("#title");
+const taskDescription = document.querySelector("#description");
+const taskDueDate = document.querySelector("#dueDate");
+const taskHighPriority = document.querySelector("#highPriority");
+const taskLowPriority = document.querySelector("#lowPriority");
+
 class toDos {
 	constructor(title, description, dueDate, priority) {
 		this.title = title;
@@ -17,21 +23,7 @@ class toDos {
 	}
 }
 
-const defaultItem = new toDos(
-	"Climbing",
-	"Go climbing at least 5 days a week",
-	"Everyday",
-	"High Priority"
-);
-
 todoItems.appendChild(listItems);
-const itemDefault = document.createElement("li");
-listItems.appendChild(itemDefault);
-
-itemDefault.innerText = `Title:${defaultItem.title}
-Description:${defaultItem.description} 
-Due Date:${defaultItem.dueDate} 
-Priority:${defaultItem.priority}`;
 
 addProject.addEventListener("click", () => {
 	projInput.removeAttribute("hidden");
@@ -41,4 +33,20 @@ cancelButton.addEventListener("click", () => {
 	projInput.hidden = true;
 });
 
-submitButton.addEventListener("click", () => {});
+submitButton.addEventListener("click", () => {
+	const itemDefault = document.createElement("li");
+	listItems.appendChild(itemDefault);
+	const newToDo = new toDos(
+		taskTitle.value,
+		taskDescription.value,
+		taskDueDate.value,
+		taskHighPriority.value,
+		taskLowPriority.value
+	);
+	itemDefault.innerText = `Title:${newToDo.title}
+    Description:${taskDescription.value}
+    Due Date:${taskDueDate.value}
+    Priority:${taskHighPriority.value}
+    OtherPrj: ${taskLowPriority.value}`;
+	projInput.hidden = true;
+});
